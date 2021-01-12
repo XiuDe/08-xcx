589d64a8d8e62e192baed4740418697847448409
zhangxiude <webxiude@163.com>
2021-01-12 11:57:51 +0800

    自定义标题栏

---
+++ 09_xcx_弹窗遮罩_自定义标题栏/pages/customTitle/customTitle.js
@@ -0,0 +1,75 @@
+// pages/customTitle/customTitle.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    jnPosition: {}, // 胶囊位置基本信息 自定义标题栏
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    // 自定义标题栏start
+    let jnPosition = wx.getMenuButtonBoundingClientRect();
+    this.setData({
+      jnPosition
+    });
+    console.log('胶囊位置', jnPosition);
+    // 自定义标题栏end
+  },
+  // 返回
+  backpage(){
+    wx.navigateBack({})
+  },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
